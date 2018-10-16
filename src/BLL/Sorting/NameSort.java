35595326239027557fd1120fbc0cd9591cae6862
StephanSchroder <36385144+StephanSchroder@users.noreply.github.com>
2018-10-16 16:09:41 +0200

    Sorting Types

---
+++ src/BLL/Sorting/NameSort.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL.Sorting;
+
+import BLL.Stock;
+import BLL.User;
+import java.util.Comparator;
+
+/**
+ *
+ * @author Stephan
+ */
+public class NameSort implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        if (o1 instanceof  User) {
+            User user1 = (User) o1;
+            User user2 = (User) o2;
+            return user1.getFirstName().compareTo(user2.getFirstName());
+        }
+        if (o1 instanceof Stock) {
+            Stock stock1 = (Stock) o1;
+            Stock stock2 = (Stock) o2;
+            return stock1.getItemName().compareTo(stock2.getItemName());
+        }
+        return -2;
+    }
+    
+}
---
+++ src/BLL/Sorting/SortSurname.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BLL.Sorting;
+
+import BLL.User;
+import java.util.Comparator;
+
+/**
+ *
+ * @author Stephan
+ */
+public class SortSurname implements Comparator {
+    
+    @Override
+    public int compare(Object o1, Object o2) {
+       User user1 = (User) o1;
+            User user2 = (User) o2;
+            return user1.getLastName().compareTo(user2.getLastName());
+    }
+}
