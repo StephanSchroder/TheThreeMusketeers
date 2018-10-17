670b3205279dc6c2c9d78bea20c066b2f0fa024c
Nico Celliers <nicocelliers97@gmail.com>
2018-10-17 22:50:12 +0200

    Category added read functions

--- src/BLL/Category.java
+++ src/BLL/Category.java
@@ -6,8 +6,10 @@
 package BLL;
 
 import DAL.*;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -51,6 +53,29 @@ public class Category {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+    
+    public List<Category> getCategories(){
+        List<Category> categories = new ArrayList<>();
+        String[][] dbData = DataHandler.readRecords(Arrays.asList("CategoryID", "CategoryName", "Description"), Arrays.asList(new DataTablesCollection("Category")), Arrays.asList());
+        int count = dbData.length;
+        for (int i = 0; i < count; i++) {
+            categories.add(new Category(Integer.valueOf(dbData[i][1]), dbData[i][2], dbData[i][3]));
+        }
+
+        return categories;
+    }
+    
+    public Category getCategory(int categoryID){
+        Category category = null;
+        String[][] dbData = DataHandler.readRecords(Arrays.asList("CategoryID", "CategoryName", "Description"), Arrays.asList(new DataTablesCollection("Category")), Arrays.asList("CategoryID=" + categoryID));
+        int count = dbData.length;
+        if (count == 1)
+        {
+            category = new Category(Integer.valueOf(dbData[0][1]), dbData[0][2], dbData[0][3]);
+        }
+
+        return category;
     }
     
     public void registerCategory() {
